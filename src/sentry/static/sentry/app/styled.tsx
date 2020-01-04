3becd96f5b55c98d884b52434febd45f4db0b590
Billy Vong <billy@sentry.io>
2020-01-04 11:55:55 -0800

    fix lint issues

--- src/sentry/static/sentry/app/styled.tsx
+++ src/sentry/static/sentry/app/styled.tsx
@@ -1,4 +1,5 @@
-import styled, {CreateStyled} from '@original-emotion/styled';
+// TODO(ts): Figure out why eslint chokes on this import
+import styled, {CreateStyled} from '@original-emotion/styled'; // eslint-disable-line import/named
 import theme from './utils/theme';
 
 export default styled as CreateStyled<typeof theme>;
--- src/sentry/static/sentry/app/views/auth/login.jsx
+++ src/sentry/static/sentry/app/views/auth/login.jsx
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import styled from '@emotion/styled';
-import {css} from '@emotion/core';
 
 import {t} from 'app/locale';
 import LoadingError from 'app/components/loadingError';
